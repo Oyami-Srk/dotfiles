9bbd81a8967c3fa3e6524237987d1099c5413d35
Shiroko <hhx.xxm@gmail.com>
2023-11-16 14:33:14 +0800

    remove pyenv, nvm and pnpm

--- .config/profile.d/env.sh
+++ .config/profile.d/env.sh
@@ -1,18 +1,6 @@
 # TODO: improve performance
 
-if [[ -d "$HOME/.pyenv" ]]; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-fi
-
 [[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
-
-if [[ -d "$HOME/.config/nvm" ]]; then
-    export NVM_DIR="$HOME/.config/nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-fi
 
 if [[ -d "/usr/local/lib/node_modules/@hyperupcall/autoenv/" ]]; then
     function load_autoenv() {
@@ -23,12 +11,4 @@ if [[ -d "/usr/local/lib/node_modules/@hyperupcall/autoenv/" ]]; then
         fi
     }
     load_autoenv
-fi
-
-if [[ -d "$HOME/.local/share/pnpm" ]]; then
-    export PNPM_HOME="$HOME/.local/share/pnpm"
-    case ":$PATH:" in
-    *":$PNPM_HOME:"*) ;;
-    *) export PATH="$PNPM_HOME:$PATH" ;;
-    esac
 fi
